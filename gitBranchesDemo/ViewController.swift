2907cc0887fea8e8a545e8d39a61c0391f8f0644
纪志刚 <jonathanji@ivygate.cn>
2018-08-21 14:00:31 +0800

    -a

--- gitBranchesDemo/ViewController.swift
+++ gitBranchesDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("我是dev分支")
         print("我是紧急修复分支")
+        print("我是release分支")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
