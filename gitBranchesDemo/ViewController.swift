a8faf2e4c129a4b9ae4c0f001b8598cae06cfb23
纪志刚 <jonathanji@ivygate.cn>
2018-08-21 11:09:45 +0800

    hotfix branch commit

--- gitBranchesDemo/ViewController.swift
+++ gitBranchesDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("我是紧急修复分支")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
